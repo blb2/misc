65b52587b7e8298c2fb5408f27334b0f07f3550d
Brian Brice <bbrice@gmail.com>
2012-05-23 11:02:38 -0500

    added update-repos.sh

---
+++ update-repos.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+shopt -s extglob
+
+BASE=$(pwd)
+TOOLS=
+
+type -P hg  &> /dev/null && TOOLS="hg  $TOOLS"
+type -P svn &> /dev/null && TOOLS="svn $TOOLS"
+type -P git &> /dev/null && TOOLS="git $TOOLS"
+
+function update
+{
+	local CWD=$(pwd)
+	local DIR=${CWD/#$BASE*(\/)/}
+
+	[ ! -z "$DIR" ] && DIR="${DIR}/"
+
+	for PROJ in `ls`; do
+		local FOUND=false
+
+		for SCM in $TOOLS; do
+			if [ -d "$PROJ" -a -d "${PROJ}/.${SCM}" ]; then
+				pushd "$PROJ" > /dev/null
+
+				case "$SCM" in
+					'git')
+						echo "git pull ${DIR}${PROJ} ..."
+						git pull
+						;;
+					'svn')
+						echo "svn update ${DIR}${PROJ} ..."
+						svn update
+						;;
+					'hg')
+						echo "hg update ${DIR}${PROJ} ..."
+						hg update
+						;;
+				esac
+
+				popd > /dev/null
+
+				FOUND=true
+				break
+			fi
+		done
+
+		if ! $FOUND && [ -d "$PROJ" ]; then
+			pushd "$PROJ" > /dev/null
+			update
+			popd > /dev/null
+		fi
+	done
+}
+
+[ ! -z "$TOOLS" ] && update
