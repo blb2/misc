3f39525ba7e2a91aa5c2b3dd6883ee40dd667e64
Brian Brice <bbrice@gmail.com>
2013-04-29 02:29:25 -0500

    add sieve.py

---
+++ sieve.py
@@ -0,0 +1,15 @@
+from math import sqrt
+
+n = 49
+sieve = [ True ] * n
+
+print ">>", int(sqrt(n))
+
+for i in xrange(2, int(sqrt(n))):
+    if sieve[i]:
+        for j in xrange(i * 2, n, i):
+            sieve[j] = False
+
+for i, v in enumerate(sieve):
+    if i > 1 and v:
+        print i
