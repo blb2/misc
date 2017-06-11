e9bb552307794dfc321238a052120f3a04cb3078
Brian Brice <bbrice@gmail.com>
2017-06-11 02:53:24 -0500

    add an example for using realpath()

---
+++ c/fullpath.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++) {
+		char* path = realpath(argv[i], NULL);
+		if (path) {
+			printf("%s -> %s\n", argv[i], path);
+			free(path);
+		} else {
+			perror(argv[i]);
+		}
+	}
+
+	return 0;
+}
