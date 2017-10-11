5cc17af1ce61debfa5c6ce6fab3f70365c2e7b06
Brian Brice <bbrice@gmail.com>
2017-10-11 12:19:46 -0500

    support Visual Studio's printf format specifier for size_t

--- c/sizes.c
+++ c/sizes.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+#ifdef _MSC_VER
+#define fmt "%Iu"
+#else
+#define fmt "%zu"
+#endif
+
 int main(void)
 {
-	printf("short\t\t%zu\n", sizeof(short));
-	printf("int\t\t%zu\n", sizeof(int));
-	printf("long\t\t%zu\n", sizeof(long));
-	printf("long long\t%zu\n", sizeof(long long));
-	printf("void*\t\t%zu\n", sizeof(void*));
+	printf("short\t\t" fmt "\n", sizeof(short));
+	printf("int\t\t" fmt "\n", sizeof(int));
+	printf("long\t\t" fmt "\n", sizeof(long));
+	printf("long long\t" fmt "\n", sizeof(long long));
+	printf("void*\t\t" fmt "\n", sizeof(void*));
 	return 0;
 }
